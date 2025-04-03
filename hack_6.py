82bdc4a693f3d458fc5a14b397721813edee9b60
Randy Parra <randyparra@hotmail.com>
2025-04-03 13:32:59 -0400

    Hack 6 listo

--- hack_6.py
+++ hack_6.py
@@ -11,5 +11,16 @@ text: [] output => ["0"]
 
 def fn_hack_6(s):
     result = s
-    #...
+    _lst = []
+
+    if len(result) > 0:
+        for index in range(len(result)):
+            if (index+1) % 2 != 0:
+                _lst.append(str(index+1))
+            else:
+                _lst.append("-")
+    else:
+        _lst.append("0")
+
+    result = _lst
     return result
