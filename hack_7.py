5ba4cdb70e39211d9660f165a700e74b8f1a5286
Randy Parra <randyparra@hotmail.com>
2025-04-03 13:36:20 -0400

    Hack 7 listo

--- hack_7.py
+++ hack_7.py
@@ -11,5 +11,16 @@ text: [0] output => [0]
 
 def fn_hack_7(s):
     result = s
-    #...
+    _lst = []
+
+    if len(result) > 1:
+        for index in range(len(result)):
+            if (index+1) % 2 != 0:
+                _lst.append(str(index+1))
+            else:
+                _lst.append(index+1)
+    else:
+        _lst = result
+
+    result = _lst
     return result
