ffb928e23498218ccf75bf49e16b8423cbdb011d
Randy Parra <randyparra@hotmail.com>
2025-04-03 11:25:18 -0400

    Hack 4 listo

--- hack_4.py
+++ hack_4.py
@@ -9,5 +9,9 @@ text: "qux" output => "qux"
 
 def fn_hack_4(s):
     result = s
-    #...
+    lastpos = (len(result)-1)
+        
+    if len(result) > 3:
+        result = result[1:lastpos]
+
     return result
