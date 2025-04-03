44165a2ede9b8c8753388fc6f7a9990d8f09ede1
Randy Parra <randyparra@hotmail.com>
2025-04-03 16:59:50 -0400

    Hack 8 listo

--- hack_8.py
+++ hack_8.py
@@ -10,5 +10,18 @@ text: ["a","b"] output => ["2","1"]
 
 def fn_hack_8(s):
     result = s
-    #...
+    revres = result
+    revres.reverse()
+    _str = []
+    index_list = [str(i+1) for i in range(len(result))]
+    index_list.reverse()
+
+    if len(result) == 4 or len(result) == 2:
+        _str = index_list
+    else:
+        for j in range(len(result)):
+            _str.append(f"{revres[j]}-{index_list[j]}")
+        
+
+    result = _str
     return result
