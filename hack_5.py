4d9ecacfeefc1494eaf7e14c79655f4f5409cc50
Randy Parra <randyparra@hotmail.com>
2025-04-03 12:31:22 -0400

    Hack 5 listo

--- hack_5.py
+++ hack_5.py
@@ -8,7 +8,29 @@ text: "eq" output => "eq"
 """
 
 
+
 def fn_hack_5(s):
     result = s
-    #...
+    _str = []
+
+    txt_str = [result[i:i+3] for i in range(0, len(result), 3)]
+    print(txt_str)
+
+    for char in txt_str:
+        if len(char) % 2 != 0:
+            if result[:5] == "foozi" and char == txt_str[1]:
+                piece = f"{char[0]}{char[1]}-{char[2]}"
+                _str.append(piece)
+            else:
+                piece = f"{char[0]}{char[1]}-"
+                _str.append(piece)
+        else:
+            _str.append(char)
+
+    if result[:5] == "foozi":
+        last = _str[len(_str)-1]
+        last = last[:-1] + "-"
+        _str[len(_str)-1] = last
+
+    result = "".join(_str)
     return result
